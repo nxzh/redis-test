90d8a9b1e75ecd0c6781775e8f6e135f9752a84e
sid.a.zhang <sid.a.zhang@capgemini.com>
2019-04-23 18:00:19 +0800

    test

--- article-score-spring/src/main/java/fun/code4/redis/Application.java
+++ article-score-spring/src/main/java/fun/code4/redis/Application.java
@@ -1,12 +1,30 @@
 package fun.code4.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+  @Autowired
+  private RedisTemplate stringRedisTemplate;
+
+  public void test() {
+    System.out.println(stringRedisTemplate);
+  }
 
   public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
 
+  @Override
+  public void run(String... args) throws Exception {
+    Application app = new Application();
+    app.test();
   }
 }
