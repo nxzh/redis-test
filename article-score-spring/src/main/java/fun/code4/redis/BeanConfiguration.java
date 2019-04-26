c451b35d1214ff7fd9604e2011fedbeff4908af1
sid.a.zhang <sid.a.zhang@capgemini.com>
2019-04-26 13:27:34 +0800

    RedisConnectionPool setting

--- article-score-spring/src/main/java/fun/code4/redis/BeanConfiguration.java
+++ article-score-spring/src/main/java/fun/code4/redis/BeanConfiguration.java
@@ -1,10 +1,12 @@
 package fun.code4.redis;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -14,12 +16,31 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class BeanConfiguration {
 
   @Bean
-  public LettuceConnectionFactory redisConnectionFactory() {
+  public GenericObjectPoolConfig genericObjectPoolConfig() {
+    GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+    genericObjectPoolConfig.setMaxIdle(2);
+    genericObjectPoolConfig.setMaxTotal(2);
+    genericObjectPoolConfig.setMinIdle(1);
+    genericObjectPoolConfig.setMaxWaitMillis(-1);
+    return genericObjectPoolConfig;
+  }
+
+  @Bean
+  public LettucePoolingClientConfiguration poolingClientConfiguration(
+      GenericObjectPoolConfig genericObjectPoolConfig) {
+    return LettucePoolingClientConfiguration.builder().poolConfig(genericObjectPoolConfig).build();
+  }
+
+  @Bean
+  public LettuceConnectionFactory redisConnectionFactory(
+      LettucePoolingClientConfiguration poolingConfig) {
     RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("10.61.213.105", 8087);
     config.setDatabase(1);
     config.setPassword("P@s5word");
-    LettuceConnectionFactory lcf = new LettuceConnectionFactory(config);
-//    lcf.setShareNativeConnection(false);
+//    LettuceConnectionFactory lcf = new LettuceConnectionFactory(config);
+    LettuceConnectionFactory lcf = new LettuceConnectionFactory(config, poolingConfig);
+    lcf.setShareNativeConnection(false);
+    lcf.setDatabase(2);
     return lcf;
   }
 
